1993c8d57727db1374cacb1f1cddc6d4c38af9a3
tapish1511 <tapishkatiyar0@gmail.com>
2025-05-25 19:17:34 +0530

    docker file updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { InitializeApp } from './data/settings.js'
 import { config } from 'dotenv'
 config();
  
-
+await InitializeApp();
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -17,7 +17,7 @@ app.prepare().then(async () => {
     handle(req, res, parsedUrl)
   }).listen(port)
   
-  await InitializeApp();
+  
   console.log(
     `> Server listening at http://localhost:${port} as ${
       dev ? 'development' : process.env.NODE_ENV
